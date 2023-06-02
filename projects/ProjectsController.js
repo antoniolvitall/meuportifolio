691ada87e845e7f87bc374e1bf28be06debebbb8
Antonio Lindo <antoniolvitall@gmail.com>
2023-06-01 21:32:23 -0300

    criando rotas

---
+++ projects/ProjectsController.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/projects", (req, res) => {
+    res.send("ROTA DE PROJETOS")
+});
+
+router.get("/admin/projects/new", (req, res) => {
+    res.send("ROTA PARA CRIAR NOVO POST DE PROJETO")
+});
