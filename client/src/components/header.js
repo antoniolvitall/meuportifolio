700383c4ec0a4ddb49a2e1f637f1dd4ee00e7440
Antonio Lindo <antoniolvitall@gmail.com>
2023-06-09 17:18:39 -0300

    excluindo arquivos desnecessarios

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,8 +1,42 @@
 import React from 'react'
+import Img from 'react-image'
+import Allogo from './public/allogo.png'
 
 const Header = () => {
   return (
-    <div>header</div>
+    <div>
+        <body>
+
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div class="container">
+                    <a class="navbar-brand" href="#">
+                        <Img src={Allogo}/>
+                        Antonio Lindo
+                    </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                        <ul class="navbar-nav ml-auto">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Welcome</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">About and Technologies</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Projects</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </body>
+    </div>
   )
 }
 
