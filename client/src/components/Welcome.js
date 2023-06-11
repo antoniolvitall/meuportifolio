44d3cfec1a5201afa32880ff16002ee5ac170337
Antonio Lindo <antoniolvitall@gmail.com>
2023-06-11 18:08:15 -0300

    finalizando pagina welcome

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Welcome.css';
-
+import { FaLinkedin, FaGithubSquare, FaFileDownload,  } from 'react-icons/fa';
 
 const Welcome = () => {
   return (
@@ -8,7 +8,9 @@ const Welcome = () => {
         <h4 className='mb-3'>Hi, I am<br/>Antonio Lindo</h4>
         <h1 className='texto-grande mb-3'>Fullstack<br/>Developer</h1>
         <div>
-            BsLinkedin
+            <a class="icon" href='https://www.linkedin.com/in/antonio-luiz-vital-lindo/'><FaLinkedin/></a>
+            <a class="icon" href='https://github.com/antoniolvitall'><FaGithubSquare/></a>
+            <a class="icon" href='https://drive.google.com/file/d/1iOMe0qSLxi5cqMosMCsECyW5nUMYAD6O/view?usp=drive_link'><FaFileDownload/></a>
         </div>
     </div>
   )
