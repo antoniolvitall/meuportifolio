7875931bb9cd357c435b46eb8e097cb737840cb8
Antonio Lindo <antoniolvitall@gmail.com>
2023-06-11 17:37:27 -0300

    instalando react icons

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Welcome.css';
-import { MDBIcon } from 'mdb-react-ui-kit';
+
 
 const Welcome = () => {
   return (
@@ -8,9 +8,7 @@ const Welcome = () => {
         <h4 className='mb-3'>Hi, I am<br/>Antonio Lindo</h4>
         <h1 className='texto-grande mb-3'>Fullstack<br/>Developer</h1>
         <div>
-            <MDBIcon fas icon="linkedin" />
-            <MDBIcon fas icon="github-square" />
-            <MDBIcon fas icon="file-download" />
+            BsLinkedin
         </div>
     </div>
   )
