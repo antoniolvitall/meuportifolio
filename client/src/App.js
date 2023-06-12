a2d0511829ad758f9e3350c05ad9945e8b219e7a
Antonio Lindo <antoniolvitall@gmail.com>
2023-06-12 19:43:55 -0300

    criando componente about

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/header';
 import Welcome from './components/Welcome';
+import About from './components/About';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div>
       <Header/>
       <Welcome/>
+      <About/>
     </div>
   )
 }
