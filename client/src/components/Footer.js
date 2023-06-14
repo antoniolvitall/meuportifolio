219b2fd7b644018310e96ccf705eb8eaf74e75ef
Antonio Lindo <antoniolvitall@gmail.com>
2023-06-13 21:16:54 -0300

    Finalizando footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,9 +1,39 @@
 import React from 'react';
 import './Footer.css';
+import {
+    MDBFooter,
+    MDBContainer,
+    MDBIcon,
+    MDBBtn
+  } from 'mdb-react-ui-kit';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <MDBFooter className='bg-dark text-center text-white'>
+      <MDBContainer className='p-4 pb-0'>
+        <section className='mb-4'>
+
+          <MDBBtn outline color="light" floating className='m-1' href='mailto:antoniolvitall@gmail.com' role='button'>
+            <MDBIcon fab icon='google' />
+          </MDBBtn>
+
+          <MDBBtn outline color="light" floating className='m-1' href='https://www.linkedin.com/in/antonio-luiz-vital-lindo/' role='button'>
+            <MDBIcon fab icon='linkedin-in' />
+          </MDBBtn>
+
+          <MDBBtn outline color="light" floating className='m-1' href='https://github.com/antoniolvitall' role='button'>
+            <MDBIcon fab icon='github' />
+          </MDBBtn>
+        </section>
+      </MDBContainer>
+
+      <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+        © 2020 Copyright:
+        <a className='text-white' href='https://mdbootstrap.com/'>
+          MDBootstrap.com
+        </a>
+      </div>
+    </MDBFooter>
   )
 }
 
