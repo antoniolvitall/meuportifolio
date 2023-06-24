f87db129e771161b3dfd7d365cb80a883d5067b1
Antonio Lindo <antoniolvitall@gmail.com>
2023-06-23 21:58:15 -0300

    minor changes

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -28,7 +28,7 @@ const Footer = () => {
       </MDBContainer>
 
       <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-        © 2023 Antonio Luiz Vital Lindo:
+        © 2023 Antonio Luiz Vital Lindo
         <a className='text-white' href='https://mdbootstrap.com/'>
           Meu site
         </a>
