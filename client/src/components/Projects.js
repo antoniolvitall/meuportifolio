3785a808b9de037f1db4032c3a271a1ddf7d34f7
Antonio Lindo <antoniolvitall@gmail.com>
2023-06-26 22:06:19 -0300

    criando colunas onde ficarao os projetos

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -2,10 +2,16 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div>
-        meus projetos
-
+  <div class="container text-center">
+    <div class="row">
+      <div class="col">
+      1 of 2
+      </div>
+    <div class="col">
+      2 of 2
     </div>
+  </div>
+  </div>
   )
 }
 
