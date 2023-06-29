355b7f98a62169796a3d7ea3b19d76387e8e342d
Antonio Lindo <antoniolvitall@gmail.com>
2023-06-28 22:35:36 -0300

    criando parte de projetos

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -1,17 +1,28 @@
-import React from 'react'
+import React from 'react';
+import './Projects.css';
 
 const Projects = () => {
   return (
-  <div class="container text-center">
-    <div class="row">
-      <div class="col">
-      1 of 2
+    <div class="projects row">
+      <div class="col-sm-6">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Special title treatment</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
       </div>
-    <div class="col">
-      2 of 2
+      <div class="col-sm-6">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Special title treatment</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-  </div>
   )
 }
 
